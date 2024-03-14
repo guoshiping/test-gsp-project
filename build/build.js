9077f1b67e9292187623d749e146a12721aa7ad9
郭世平 <shiping.guo@getech.cn>
2024-03-14 15:33:20 +0800

    feat: 测试ci流程

--- build/build.js
+++ build/build.js
@@ -1,30 +1,31 @@
-const shell = require('shelljs');
-const chalk = require('chalk');
-const fs = require('fs-extra');
-const path = require('path');
-const Base = require('./base');
+const shell = require("shelljs");
+const chalk = require("chalk");
+const fs = require("fs-extra");
+const path = require("path");
+const Base = require("./base");
 
+// 注释一下
 class Build extends Base {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    run() {
-        this.init();
-        this.build();
-    }
+  run() {
+    this.init();
+    this.build();
+  }
 
-    init() {
-        fs.emptyDirSync(path.join(this.rootPath, 'lib'));
-    }
+  init() {
+    fs.emptyDirSync(path.join(this.rootPath, "lib"));
+  }
 
-    build() {
-        shell.exec('tsc');
-    }
+  build() {
+    shell.exec("tsc");
+  }
 
-    logError(message) {
-        console.log(chalk.red(message));
-    }
+  logError(message) {
+    console.log(chalk.red(message));
+  }
 }
 
 new Build().run();
